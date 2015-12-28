31fe92bb7317a4eaaf7cdf7cfe965e98f39d44bd
chenxuuu <liucx1@qq.com>
2015-12-28 21:24:10 +0800

    微调设置

--- str.php
+++ str.php
@@ -22,7 +22,7 @@ if(!strstr($urlget, 'http://'))
 
 
 function shorturl($long_url){
-	$apiKey='3213676317';//要修改这里的key再测试哦
+	$apiKey='3738750665';//要修改这里的key再测试哦
 	$apiUrl='http://api.t.sina.com.cn/short_url/shorten.json?source='.$apiKey.'&url_long='.$long_url;
 	$response = file_get_contents($apiUrl);
 	$json = json_decode($response);
@@ -30,7 +30,7 @@ function shorturl($long_url){
 }
 
 function expandurl($short_url){
-	$apiKey='3213676317';//要修改这里的key再测试哦
+	$apiKey='3738750665';//要修改这里的key再测试哦
 	$apiUrl='http://api.t.sina.com.cn/short_url/expand.json?source='.$apiKey.'&url_short='.$short_url;
 
 	$response = file_get_contents($apiUrl);
